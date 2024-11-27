8a4b77aae715fedab3552a819a4547e5a24026a5
enzocomics <enzo@cheerupemokid.com>
2024-11-27 12:18:10 -0800

    allow style tag to be null

--- src/AppProgressBar.tsx
+++ src/AppProgressBar.tsx
@@ -23,15 +23,98 @@ type PushStateInput = [
 
 export const AppProgressBar = React.memo(
 	({
+		color = '#0A2FFF',
+		height = '2px',
 		options,
 		shallowRouting = false,
 		disableSameURL = true,
 		startPosition = 0,
 		delay = 0,
 		stopDelay = 0,
+		style,
+		nonce,
 		targetPreprocessor,
 		disableAnchorClick = false,
 	}: ProgressBarProps) => {
+		const styles = (
+			<style nonce={nonce}>
+				{style ||
+					`
+          #nprogress {
+            pointer-events: none;
+          }
+
+          #nprogress .bar {
+            background: ${color};
+
+            position: fixed;
+            z-index: 99999;
+            top: 0;
+            left: 0;
+
+            width: 100%;
+            height: ${height};
+          }
+
+          /* Fancy blur effect */
+          #nprogress .peg {
+            display: block;
+            position: absolute;
+            right: 0px;
+            width: 100px;
+            height: 100%;
+            box-shadow: 0 0 10px ${color}, 0 0 5px ${color};
+            opacity: 1.0;
+
+            -webkit-transform: rotate(3deg) translate(0px, -4px);
+                -ms-transform: rotate(3deg) translate(0px, -4px);
+                    transform: rotate(3deg) translate(0px, -4px);
+          }
+
+          /* Remove these to get rid of the spinner */
+          #nprogress .spinner {
+            display: block;
+            position: fixed;
+            z-index: 1031;
+            top: 15px;
+            right: 15px;
+          }
+
+          #nprogress .spinner-icon {
+            width: 18px;
+            height: 18px;
+            box-sizing: border-box;
+
+            border: solid 2px transparent;
+            border-top-color: ${color};
+            border-left-color: ${color};
+            border-radius: 50%;
+
+            -webkit-animation: nprogress-spinner 400ms linear infinite;
+                    animation: nprogress-spinner 400ms linear infinite;
+          }
+
+          .nprogress-custom-parent {
+            overflow: hidden;
+            position: relative;
+          }
+
+          .nprogress-custom-parent #nprogress .spinner,
+          .nprogress-custom-parent #nprogress .bar {
+            position: absolute;
+          }
+
+          @-webkit-keyframes nprogress-spinner {
+            0%   { -webkit-transform: rotate(0deg); }
+            100% { -webkit-transform: rotate(360deg); }
+          }
+          @keyframes nprogress-spinner {
+            0%   { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        ` || null}
+			</style>
+		);
 
 		NProgress.configure(options || {});
 
@@ -182,7 +265,7 @@ export const AppProgressBar = React.memo(
 			disableSameURL,
 		]);
 
-		return null;
+		return styles;
 	},
 	(prevProps, nextProps) => {
 		if (nextProps?.memo === false) {
